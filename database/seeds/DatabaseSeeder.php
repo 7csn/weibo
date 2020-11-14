77b37f61c9e6b8b2b93c92335a83fa78666a93f1
7csn <1479672753@qq.com>
2020-11-14 07:53:13 +0000

    车着急用户列表

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,16 +1,16 @@
 <?php
 
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     *
-     * @return void
-     */
     public function run()
     {
-        // $this->call(UserSeeder::class);
+        Model::unguard();
+
+        $this->call(UsersTableSeeder::class);
+
+        Model::reguard();
     }
 }
---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+
+class UsersTableSeeder extends Seeder
+{
+    public function run()
+    {
+        factory(User::class)->times(50)->create();
+
+        $user = User::find(1);
+        $user->name = '7csn';
+        $user->email = '1479672753@qq.com';
+        $user->save();
+    }
+}
